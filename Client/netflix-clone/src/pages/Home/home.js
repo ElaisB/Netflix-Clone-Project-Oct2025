1a4fb9e07873dd19df18e3011c3a89ef1fa91477
EliasB <eliaszemedkun3@gmail.com>
2025-10-16 19:23:38 +0300

    Banner

--- Client/netflix-clone/src/pages/Home/home.js
+++ Client/netflix-clone/src/pages/Home/home.js
@@ -1,12 +1,16 @@
 import React from 'react'
 import Header from '../../Components/Header/Header'
 import Footer from '../../Components/Footer/Footer'
+import Banner from '../../Components/Banner/Banner'
 
 function home() {
   return (
     <>
         <Header/>
+        <Banner/>
         <Footer/>
+        
+
     </>
 
   )
