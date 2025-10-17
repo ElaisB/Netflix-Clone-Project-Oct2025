7032a56bd21ebe8b6a0030d3d85b1b380272fd5c
ElaisB <eliaszemedkun3@gmail.com>
2025-10-17 18:15:53 +0300

    Deploy3

---
+++ src/pages/Home/home.js
@@ -0,0 +1,22 @@
+import React from 'react'
+import Header from '../../Components/Header/Header'
+import Footer from '../../Components/Footer/Footer'
+import Banner from '../../Components/Banner/Banner'
+import RowList from '../../Components/Rows/Rowlist/RowList'
+
+function home() {
+  return (
+    <>
+        <Header/>
+        <Banner/>
+        <RowList/>
+        <Footer/>
+        
+
+    </>
+
+  )
+}
+
+
+export default home
